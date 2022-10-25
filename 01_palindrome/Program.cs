e6c6494155b819e247e1fc81df198b4121e7124c
Anna <ann.sxd@example.com>
2022-10-25 18:10:00 +0300

    1 задача

--- 01_palindrome/Program.cs
+++ 01_palindrome/Program.cs
@@ -4,19 +4,19 @@
 Console.Write("Введите пятизначное число: ");
 int number = int.Parse(Console.ReadLine() ?? "0");
 
-int [] array = new int[5];          // массив из 5 элементов
-int index = 4;                      
-while(index >= 0)
+int[] array = new int[5];           // массив из 5 элементов
+int index = 4;                      // считаем элементы с конца
+while (index >= 0)
 {
-    array[index] = number%10;       
-    number = number/10;            
-    index--;                        
+    array[index] = number % 10;     //каждому элементу массива присваивается последняя цифра числа
+    number = number / 10;
+    index--;
 }
-if(array[0] == array[4] && array[1] == array[3])
+if (array[0] == array[4] && array[1] == array[3])
 {
     Console.WriteLine($"Число является палиндромом");
 }
-else 
+else
 {
     Console.WriteLine($"Число НЕявляется палиндромом");
 }
