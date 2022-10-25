c0baca9300a798c760e9ad84f42fefdba3517d7b
Anna <ann.sxd@example.com>
2022-10-25 21:49:38 +0300

    4 задача

--- 04_prime_numbers/Program.cs
+++ 04_prime_numbers/Program.cs
@@ -2,27 +2,27 @@
 
 bool IsPrimeNumb(int number)        // функция на проверку простого числа
 {
-    if(number == 1)                // 1 - не простое число
-    return false;
+    if (number == 1)                // 1 - не простое число
+        return false;
 
-    for(int i=2; i < number; i++)       // проверяем простое ли число 
+    for (int i = 2; i < number; i++)       // проверяем простое ли число 
     {
-        if(number % i == 0) return false;
+        if (number % i == 0) return false;
     }
-    return true;                        
+    return true;
 }
 
 Console.Clear();
 Console.Write("Введите число: ");
 int result;
-if(!int.TryParse(Console.ReadLine(), out result) || (result < 0))       // если число будет введено не интовое либо отрицательное
+if (!int.TryParse(Console.ReadLine(), out result) || (result < 0))       // если число будет введено не интовое либо отрицательное
 {
     Console.WriteLine("Введите целое положительное число");
 }
 
-for(int i = 1; i <= result; i++)
+for (int i = 1; i <= result; i++)
 {
-    if (IsPrimeNumb(i))                         // функция возвращает true - выводим число, false - не выводим
+    if (IsPrimeNumb(i))                        // функция возвращает true - выводим число, false - не выводим
     {
         Console.Write($"{i}, ");
     }
