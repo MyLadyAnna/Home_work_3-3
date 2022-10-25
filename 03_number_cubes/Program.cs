6e39763b81517c55451fa5c9fb0a912947f09a19
Anna <ann.sxd@example.com>
2022-10-25 19:56:57 +0300

    3 задача

---
+++ 03_number_cubes/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void PrintCubesNumbers(double number)
+{
+    double count = 1;
+    double cube = 0;
+    while (count <= number)
+    {
+
+        cube = Math.Pow(count, 3);
+        Console.Write($"{cube}, ");
+        count++;
+    }
+    return;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine() ?? "0");
+
+PrintCubesNumbers(number);
